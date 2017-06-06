e9aa243067920b58a0d5aebf17f9c1f3a420ec7c
Gaston Alberto Martinez <Xero-Hige@users.noreply.github.com>
2017-06-06 20:11:02 -0300

    Add files via upload

---
+++ Algoritmos I/2017-1C/Clase [05-06] (Compilacion en C)/src/hola_munro_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int print(){
+	printf ("Hola Munro\n");
+}
+
+int main(){
+	return print();
+}
---
+++ Algoritmos I/2017-1C/Clase [05-06] (Compilacion en C)/src/hola_munro_6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	for (int i = 0; i<5; i++){
+		printf ("Hola Munro\n");
+	}
+	return 0;
+}
